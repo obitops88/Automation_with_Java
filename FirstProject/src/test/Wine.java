7b9d3155a80b0629a5d270a17aa2e7506f33894f
Obiora Ume <Obitops88@gmail.com>
2015-10-05 20:46:47 +0100

    I added two projects

---
+++ FirstProject/src/test/Wine.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by Obi on 31/07/2015.
+ */
+public class Wine {
+
+    public static void main(String []args){
+
+        System.out.println("Hello World");
+    }
+}
---
+++ MarksAndSpencer/src/clothes/Wine.java
@@ -0,0 +1,10 @@
+package clothes;
+
+public class Wine {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Obiora");
+
+	}
+
+}
